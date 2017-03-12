6695b286ffcaf864a149dabd03fc3a171a1c51a7
mahbeb <linbub007@gmail.com>
2017-03-12 11:19:46 +0700

    seken

--- app/src/main/java/com/example/me/todolistpad/MainActivity.java
+++ app/src/main/java/com/example/me/todolistpad/MainActivity.java
@@ -26,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("");
-
+        
         setupListViewListener();
     }
 
